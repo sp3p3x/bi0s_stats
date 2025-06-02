9e831b061471c8a25c6fe598bf895665a3fdc88d
sp3p3x <yadhukk12345@gmail.com>
2025-06-02 18:35:02 +0530

    reverted some changes (build fails)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.sp3p3x.bi0s_stats"
+    namespace = "com.example.bi0s_stats"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.sp3p3x.bi0s_stats"
+        applicationId = "com.example.bi0s_stats"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
