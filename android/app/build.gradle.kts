6bbf96fd6c7fd0d71ba9e3dce080a8daa5b78a6d
sp3p3x <yadhukk12345@gmail.com>
2025-06-11 01:26:06 +0530

    udpate ndkversion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bi0s_stats"
+    namespace = "com.sp3p3x.bi0s_stats"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bi0s_stats"
+        applicationId = "com.sp3p3x.bi0s_stats"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/bi0s_stats/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bi0s_stats/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.bi0s_stats
+package com.sp3p3x.bi0s_stats
 
 import io.flutter.embedding.android.FlutterActivity
 
