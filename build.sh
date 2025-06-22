eb1e0475d9d961f6c2db2feabda74915d8675a03
sp3p3x <yadhukk12345@gmail.com>
2025-06-22 19:13:46 +0530

    build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+flutter build apk --release
+mv build/app/outputs/flutter-apk/app-release.apk builds/bi0s_stats.apk
+echo .apk built! path: builds/bi0s_stats.apk
+
+# flutter build linux --release
+# mv build/linux/x64/release/bundle/bi0s_stats builds/
