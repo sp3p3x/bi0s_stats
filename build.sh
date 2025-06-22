325211e3a253c4ed4d9e438fc7e4c61e2bf44fb2
sp3p3x <yadhukk12345@gmail.com>
2025-06-22 22:42:33 +0530

    added aab build commands

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@ flutter build apk --release
 mv build/app/outputs/flutter-apk/app-release.apk builds/bi0s_stats.apk
 echo .apk built! path: builds/bi0s_stats.apk
 
+flutter build aab --release
+mv build/app/outputs/bundle/release/app-release.aab builds/bi0s_stats.aab
+echo .aab built! path: builds/bi0s_stats.aab
 # flutter build linux --release
 # mv build/linux/x64/release/bundle/bi0s_stats builds/
